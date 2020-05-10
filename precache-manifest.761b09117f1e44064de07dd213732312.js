f8fd53817574f45c27038473994091e95a368e4c
mekandru <kandru.m@husky.neu.edu>
2020-05-10 00:30:57 -0700

    Updates

---
+++ precache-manifest.761b09117f1e44064de07dd213732312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2620ca95be35eb1173cac43d001e12c6",
+    "url": "/Shop-Organics/index.html"
+  },
+  {
+    "revision": "f7efc29ebe80d1995dfc",
+    "url": "/Shop-Organics/static/css/main.81620252.chunk.css"
+  },
+  {
+    "revision": "30b81c59cdbe6cba5a7e",
+    "url": "/Shop-Organics/static/js/2.49ab7eae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shop-Organics/static/js/2.49ab7eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7efc29ebe80d1995dfc",
+    "url": "/Shop-Organics/static/js/main.f1ec07de.chunk.js"
+  },
+  {
+    "revision": "a190bd9cebed627fc23b",
+    "url": "/Shop-Organics/static/js/runtime-main.4e0e87b1.js"
+  }
+]);
